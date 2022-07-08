6e835f4f725fbe56c8320317097a459ac4003697
Michal1898 <michal.volf@gmail.com>
2022-07-08 05:19:42 +0200

    Rest of secret code changed to list.

--- main.py
+++ main.py
@@ -1,17 +1,16 @@
 def eval_attempt(secret_code="00000", inserted_code="00000"):
     passed_digits = ""
-    secret_rest = ""
+    secret_rest = []
     inserted_rest = ""
 
     for my_pointer in range(0, len(secret_code)):
         if inserted_code[my_pointer] == secret_code[my_pointer]:
             passed_digits += secret_code[my_pointer]
         else:
-            secret_rest += secret_code[my_pointer]
+            secret_rest.append(secret_code[my_pointer])
             inserted_rest += inserted_code[my_pointer]
 
     black_stick = len(passed_digits)
-    secret_rest = list(secret_rest)
     white_stick = 0
 
     for compared_digit in inserted_rest:
